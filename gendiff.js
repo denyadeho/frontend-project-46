593fb1e947ba2365dc798e5073d121c101683a04
Popovnin <denyadeho@gmail.com>
2023-03-16 17:44:18 +0300

    test

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-
+import fs from 'node:fs';
+import path from 'path';
+import _ from "lodash";
 import { program } from 'commander';
 
 const command = () => {
     console.log('Hello, World!');
 };
-
+const file1 = fs.readFileSync('file1.json', 'utf-8');
+const JSONfile1 = JSON.parse(file1);
+console.log(JSONfile1);
 
 program
     .description('Compares two configuration files and shows a difference.')
