0191dfd3fa7eed52b5722fcb1c9fb2cdaeaa49d9
Denis <denyadeho@gmail.com>
2023-03-18 21:56:19 +0300

    added logic for gendiff

--- gendiff.js
+++ gendiff.js
@@ -7,35 +7,34 @@ import { program } from 'commander';
 const command = () => {
     console.log('Hello, World!');
 };
+
 const file1 = fs.readFileSync('file1.json', 'utf-8');
 const JSONfile1 = JSON.parse(file1);
 const file2 = fs.readFileSync('file2.json', 'utf-8');
 const JSONfile2 = JSON.parse(file2);
-console.log(Object.entries(JSONfile1))
-console.log(Object.entries(JSONfile2))
-const result = [];
-Object.entries(JSONfile1).forEach(([key1, value1]) => {
-    const a = Object.entries(JSONfile2).map(([key2, value2], index) => {
-        if (key1 === key2) {
-            if (value1 === value2) {
-                return `  ${key1}: ${value1}`
-            } else {
-                return `+ ${key2}: ${value2}`
-            }
-        }
-        if (index === Object.entries(JSONfile2).length - 1) {
-            return `- ${key1}: ${value1}`
-        }
-        if (!JSONfile1.hasOwnProperty(key2)) {
-            return `+ ${key2}: ${value2}`
-        }
-    })
-    result.push(a);
-});
-const arr = result.flat();
-const uniqArr = _.uniq(arr);
-console.log(uniqArr);
 
+const result = [];
+
+const allKeys = _.uniq([...Object.keys(JSONfile1), ...Object.keys(JSONfile2)].sort());
+
+for (const item of allKeys) {
+    if (JSONfile1[item] !== undefined && JSONfile2[item] === undefined) {
+        result.push(`- ${item}: ${JSONfile1[item]}`)
+    }
+    if (JSONfile2[item] !== undefined && JSONfile1[item] === undefined) {
+        result.push(`+ ${item}: ${JSONfile2[item]}`)
+    }
+    if (JSONfile1[item] === JSONfile2[item]) {
+        result.push(`  ${item}: ${JSONfile1[item]}`)
+    }
+    if (JSONfile1[item] !== undefined && JSONfile2[item] !== undefined && JSONfile1[item] !== JSONfile2[item]) {
+        result.push(`- ${item}: ${JSONfile1[item]}`);
+        result.push(`+ ${item}: ${JSONfile2[item]}`)
+    }
+}
+
+const text = `{\n${result.join('\n')}\n}`;
+console.log(text);
 
 program
     .description('Compares two configuration files and shows a difference.')
