a23e94a696c4d2d7a7ed3c40a24febb87ff32832
Popovnin <denyadeho@gmail.com>
2023-03-29 17:43:11 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 import fs from 'node:fs';
 import path from 'path';
 import _ from "lodash";
-const genDiff = (filepath1, filepath2) => {
-    const file1 = fs.readFileSync(filepath1, 'utf-8');
-    const JSONfile1 = JSON.parse(file1);
-    const file2 = fs.readFileSync(filepath2, 'utf-8');
-    const JSONfile2 = JSON.parse(file2);
+import jsYaml from "js-yaml";
+import parserYaml from "../parsers/parserYaml.js";
+import parserJSON from "../parsers/parserJSON.js";
+import parser from "../parsers/parser.js";
+
+const logicFunc = (file1, file2) => {
 
     const result = [];
 
-    const allKeys = _.uniq([...Object.keys(JSONfile1), ...Object.keys(JSONfile2)].sort());
+    const allKeys = _.uniq([...Object.keys(file1), ...Object.keys(file2)].sort());
 
     for (const item of allKeys) {
-        if (JSONfile1[item] !== undefined && JSONfile2[item] === undefined) {
-            result.push(`- ${item}: ${JSONfile1[item]}`)
+        if (file1[item] !== undefined && file2[item] === undefined) {
+            result.push(`- ${item}: ${file1[item]}`)
         }
-        if (JSONfile2[item] !== undefined && JSONfile1[item] === undefined) {
-            result.push(`+ ${item}: ${JSONfile2[item]}`)
+        if (file2[item] !== undefined && file1[item] === undefined) {
+            result.push(`+ ${item}: ${file2[item]}`)
         }
-        if (JSONfile1[item] === JSONfile2[item]) {
-            result.push(`  ${item}: ${JSONfile1[item]}`)
+        if (file1[item] === file2[item]) {
+            result.push(`  ${item}: ${file1[item]}`)
         }
-        if (JSONfile1[item] !== undefined && JSONfile2[item] !== undefined && JSONfile1[item] !== JSONfile2[item]) {
-            result.push(`- ${item}: ${JSONfile1[item]}`);
-            result.push(`+ ${item}: ${JSONfile2[item]}`)
+        if (file1[item] !== undefined && file2[item] !== undefined && file1[item] !== file2[item]) {
+            result.push(`- ${item}: ${file1[item]}`);
+            result.push(`+ ${item}: ${file2[item]}`)
         }
     }
 
     const text = `{\n${result.join('\n')}\n}`;
     return text;
 }
+
+
+const genDiff = (filepath1, filepath2) => {
+    const file1 = parser(filepath1);
+    const file2 = parser(filepath2);
+
+    return logicFunc(file1, file2)
+}
+
 export default genDiff;
