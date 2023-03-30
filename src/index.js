38804afdabc214624e71ab450c1b3826821599b4
Popovnin <denyadeho@gmail.com>
2023-03-30 21:59:12 +0300

    fixed code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import jsYaml from "js-yaml";
 import parserYaml from "../parsers/parserYaml.js";
 import parserJSON from "../parsers/parserJSON.js";
 import parser from "../parsers/parser.js";
-const getTextForPush = (key, value, deepLVL, sign) => {
-    return `${' '.repeat(deepLVL * 2)}${sign} ${key}: ${value}`
+const getTextForPush = (key, value, deepLVL, sign, isSetSign = true) => {
+    return `${' '.repeat(deepLVL * 4)}${isSetSign ? sign : ''} ${key}: ${value}`
 }
-const logicFunc = (file1, file2, deepLVL = 0) => {
+const logicFunc = (file1, file2, deepLVL = 0, isSetSign = true) => {
 
     const result = [];
 
@@ -18,42 +18,42 @@ const logicFunc = (file1, file2, deepLVL = 0) => {
         if (_.isObject(file1[item]) || _.isObject(file2[item])) {
             if (_.isObject(file1[item]) && _.isObject(file2[item])) {
                 const deepResult = logicFunc(file1[item], file2[item], deepLVL + 1);
-                result.push(getTextForPush(item, deepResult, deepLVL, ''));
+                    result.push(getTextForPush(item, deepResult, deepLVL, ''));
             }
             if (_.isObject(file1[item]) && !_.isObject(file2[item])) {
-                const deepResult = logicFunc(file1[item], {}, deepLVL + 1);
-                result.push(getTextForPush(item, deepResult, deepLVL, '-'));
+                const deepResult = logicFunc(file1[item], {}, deepLVL + 1, false);
+                    result.push(getTextForPush(item, deepResult, deepLVL, '-', isSetSign));
                 if (file2[item] !== undefined) {
-                    result.push(getTextForPush(item, file2[item], deepLVL, '+'))
+                    result.push(getTextForPush(item, file2[item], deepLVL, '+', isSetSign))
                 }
             }
             if (!_.isObject(file1[item]) && _.isObject(file2[item])) {
-                const deepResult = logicFunc({}, file2[item], deepLVL + 1);
+                const deepResult = logicFunc({}, file2[item], deepLVL + 1, false);
                 if (file1[item] !== undefined) {
                     result.push(getTextForPush(item, file1[item], deepLVL, '-'))
                 }
-                result.push(getTextForPush(item, deepResult, deepLVL, '+'))
+                    result.push(getTextForPush(item, deepResult, deepLVL, '+', isSetSign))
             }
         }
 
         else {
         if (file1[item] !== undefined && file2[item] === undefined) {
-            result.push(getTextForPush(item, file1[item], deepLVL, '-'))
+            result.push(getTextForPush(item, file1[item], deepLVL, '-', isSetSign))
         }
         if (file2[item] !== undefined && file1[item] === undefined) {
-            result.push(getTextForPush(item, file2[item], deepLVL, '+'))
+            result.push(getTextForPush(item, file2[item], deepLVL, '+', isSetSign))
         }
         if (file1[item] === file2[item]) {
-            result.push(getTextForPush(item, file1[item], deepLVL, ' '))
+            result.push(getTextForPush(item, file1[item], deepLVL, ' ', isSetSign))
         }
         if (file1[item] !== undefined && file2[item] !== undefined && file1[item] !== file2[item]) {
-            result.push(getTextForPush(item, file1[item], deepLVL, '-'))
-            result.push(getTextForPush(item, file2[item], deepLVL, '+'))
+            result.push(getTextForPush(item, file1[item], deepLVL, '-', isSetSign))
+            result.push(getTextForPush(item, file2[item], deepLVL, '+', isSetSign))
         }
 
         }
     }
-    const text = `{\n${result.join('\n')}\n${' '.repeat(deepLVL * 2)}}`;
+    const text = `{\n${result.join('\n')}\n${' '.repeat(deepLVL * 4)}}`;
     return text;
 }
 
