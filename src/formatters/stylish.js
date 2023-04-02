348b6df1d41dfca9aaab036fc5d4564cc2d7e1f8
Popovnin <denyadeho@gmail.com>
2023-04-03 02:28:02 +0300

    .

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -47,7 +47,7 @@ const funcStylish = (file1, file2, deepLVL = 1, isSetSign = true) => {
 
         }
     }
-    const text = `{\n${result.join('\n')}\n${' '.repeat(deepLVL * 3 - 1)}}`;
+    const text = `{\n${result.join('\n')}\n${' '.repeat(deepLVL * 3 - 2)}}`;
     return text;
 }
 
