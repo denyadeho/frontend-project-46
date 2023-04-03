4e50767cf075c491523d7326e085042960a2735f
Popovnin <denyadeho@gmail.com>
2023-04-04 02:59:56 +0300

    fixes

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,31 +12,28 @@ const funcPlain = (file1, file2, parent = '') => {
   };
   const path = (parent1, item) => (parent1 ? `${parent1}.${item}` : item);
 
-  const result = [];
   const allKeys = _.sortBy(_.uniq([...Object.keys(file1), ...Object.keys(file2)]));
   /* eslint-disable-next-line */
-  for (const item of allKeys) {
-    if (_.isObject(file1[item]) && _.isObject(file2[item])) {
-      const recursionAdd = funcPlain(file1[item], file2[item], path(parent, item));
-      result.push(recursionAdd);
-    } else {
+  const result = allKeys.map((item) => {
+    if (file1[item] !== undefined || file2[item] !== undefined) {
+      if (_.isObject(file1[item]) && _.isObject(file2[item])) {
+        const recursionAdd = funcPlain(file1[item], file2[item], path(parent, item));
+        return recursionAdd;
+      }
       if (file1[item] !== undefined && file2[item] === undefined) {
-        result.push(`Property '${path(parent, item)}' was removed`);
+        return `Property '${path(parent, item)}' was removed`;
       }
       if (file2[item] !== undefined && file1[item] === undefined) {
-        result.push(`Property '${path(parent, item)}' was added with value: ${checkType(file2[item])}`);
+        return `Property '${path(parent, item)}' was added with value: ${checkType(file2[item])}`;
       }
 
       if (file1[item] !== undefined && file2[item] !== undefined && file1[item] !== file2[item]) {
-        result.push(
-          `Property '${path(parent, item)}' was updated. From ${checkType(file1[item])} to ${checkType(
-            file2[item],
-          )}`,
-        );
+        return `Property '${path(parent, item)}' was updated. From ${checkType(file1[item])} to ${checkType(file2[item])}`;
       }
     }
-  }
-  const text = `\n${result.join('\n')}\n`;
-  return text.trim();
+  });
+  const filteredArr = result.filter((item) => item !== undefined);
+  const text = `${filteredArr.join('\n')}`;
+  return text;
 };
 export default funcPlain;
