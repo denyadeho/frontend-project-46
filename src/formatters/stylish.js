4a8bbe39ff9ca50a9894e1ffea60c439a1c0a22c
Popovnin <denyadeho@gmail.com>
2023-04-04 22:02:23 +0300

    fixes

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,46 +2,44 @@ import _ from 'lodash';
 
 const funcStylish = (file1, file2, deepLVL = 1, isSetSign = true) => {
   const getTextForPush = (key, value, sign, deepLVL1 = 1, isSetSign1 = true) => `${' '.repeat(deepLVL1 * 4 - 2)}${isSetSign1 ? sign : ' '} ${key}: ${value}`;
-  const result = [];
 
-  const allKeys = _.uniq([...Object.keys(file1), ...Object.keys(file2)].sort());
+  const allKeys = _.sortBy(_.uniq([...Object.keys(file1), ...Object.keys(file2)]));
   /* eslint-disable-next-line */
-  for (const item of allKeys) {
+  const result = allKeys.flatMap((item) => {
     if (_.isObject(file1[item]) || _.isObject(file2[item])) {
       if (_.isObject(file1[item]) && _.isObject(file2[item])) {
         const deepResult = funcStylish(file1[item], file2[item], deepLVL + 1);
-        result.push(getTextForPush(item, deepResult, ' ', deepLVL));
+        return getTextForPush(item, deepResult, ' ', deepLVL);
       }
       if (_.isObject(file1[item]) && !_.isObject(file2[item])) {
         const deepResult = funcStylish(file1[item], {}, deepLVL + 1, false);
-        result.push(getTextForPush(item, deepResult, '-', deepLVL, isSetSign));
         if (file2[item] !== undefined) {
-          result.push(getTextForPush(item, file2[item], '+', deepLVL));
+          return [getTextForPush(item, deepResult, '-', deepLVL, isSetSign), getTextForPush(item, file2[item], '+', deepLVL)];
         }
+        return getTextForPush(item, deepResult, '-', deepLVL, isSetSign);
       }
       if (!_.isObject(file1[item]) && _.isObject(file2[item])) {
         const deepResult = funcStylish({}, file2[item], deepLVL + 1, false);
         if (file1[item] !== undefined) {
-          result.push(getTextForPush(item, file1[item], '-', deepLVL));
+          return [getTextForPush(item, file1[item], '-', deepLVL), getTextForPush(item, deepResult, '+', deepLVL, isSetSign)];
         }
-        result.push(getTextForPush(item, deepResult, '+', deepLVL, isSetSign));
+        return getTextForPush(item, deepResult, '+', deepLVL, isSetSign);
       }
     } else {
       if (file1[item] !== undefined && file2[item] === undefined) {
-        result.push(getTextForPush(item, file1[item], '-', deepLVL, isSetSign));
+        return getTextForPush(item, file1[item], '-', deepLVL, isSetSign);
       }
       if (file2[item] !== undefined && file1[item] === undefined) {
-        result.push(getTextForPush(item, file2[item], '+', deepLVL, isSetSign));
+        return getTextForPush(item, file2[item], '+', deepLVL, isSetSign);
       }
       if (file1[item] === file2[item]) {
-        result.push(getTextForPush(item, file1[item], ' ', deepLVL, isSetSign));
+        return getTextForPush(item, file1[item], ' ', deepLVL, isSetSign);
       }
       if (file1[item] !== undefined && file2[item] !== undefined && file1[item] !== file2[item]) {
-        result.push(getTextForPush(item, file1[item], '-', deepLVL, isSetSign));
-        result.push(getTextForPush(item, file2[item], '+', deepLVL, isSetSign));
+        return [getTextForPush(item, file1[item], '-', deepLVL, isSetSign), getTextForPush(item, file2[item], '+', deepLVL, isSetSign)];
       }
     }
-  }
+  })
   const text = `{\n${result.join('\n')}\n${' '.repeat(deepLVL * 4 - 4)}}`;
   return text;
 };
